d7d0d0bde9cc0b69df67c07f0cd4e38d64ec2256
ShawnTseng <shawntseng40@gmail.com>
2023-09-07 16:17:51 +0800

    fix: session error

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -1,19 +1,31 @@
-import { getSession } from 'next-auth/react';
-import prisma from '../../../lib/prisma';
+import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '../../../lib/prisma'
+import { getSession } from 'next-auth/react'
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../auth/[...nextauth]';
+
 
 // POST /api/post
 // Required fields in body: title
 // Optional fields in body: content
-export default async function handle(req, res) {
+export default async function handle(req: NextApiRequest, res: NextApiResponse) {
     const { title, content } = req.body;
 
-    const session = await getSession({ req });
-    const result = await prisma.post.create({
-        data: {
-            title: title,
-            content: content,
-            author: { connect: { email: session?.user?.email } },
-        },
-    });
-    res.json(result);
+    // TODO:broken, don't know why
+    // const session = await getSession({ req });
+
+    const session = await getServerSession(req, res, authOptions)
+
+    if (session) {
+        const result = await prisma.post.create({
+            data: {
+                title: title,
+                content: content,
+                author: { connect: { email: session?.user?.email } },
+            },
+        });
+        res.json(result);
+    } else {
+        res.status(401).send({ message: 'Unauthorized' })
+    }
 }
